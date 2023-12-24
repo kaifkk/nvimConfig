22cb33644c41b9235c3a13b346b8c825c179d103
kaifkk <kaifkk>
2023-12-24 10:29:03 +0600

    Last

--- lua/kaif/plugins/lsp/mason.lua
+++ lua/kaif/plugins/lsp/mason.lua
@@ -1,31 +1,29 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		-- "jayp0521/mason-null-ls.nvim",
-		-- "jose-elias-alvarez/null-ls.nvim",
-	},
-	config = function()
-		require("mason").setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
-				},
-			},
-		})
-		require("mason-lspconfig").setup({
-			ensure_installed = {
-				"emmet_ls",
-				"tsserver",
-				"html",
-				"cssls",
-				"lua_ls",
-				"clangd",
-				"pyright",
-			},
-			automatic_installation = true,
-		})
-	end,
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+  },
+  config = function()
+    require("mason").setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    })
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        "emmet_ls",
+        "tsserver",
+        "html",
+        "cssls",
+        "lua_ls",
+        "clangd",
+        "pyright",
+      },
+      automatic_installation = true,
+    })
+  end,
 }
--- lua/kaif/plugins/nvim-lint.lua
+++ lua/kaif/plugins/nvim-lint.lua
@@ -1,19 +1,19 @@
 return {
-	"mfussenegger/nvim-lint",
-	config = function()
-		require("lint").linters_by_ft = {
-			cpp = { "clangtidy" },
-			c = { "cpplint" },
-			lua = { "luacheck" },
-			html = { "tidy" },
-			css = { "stylelint" },
-			javascript = { "eslint" },
-			py = { "pylint" },
-		}
-		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-			callback = function()
-				require("lint").try_lint()
-			end,
-		})
-	end,
+  "mfussenegger/nvim-lint",
+  config = function()
+    require("lint").linters_by_ft = {
+      cpp = { "clangtidy" },
+      c = { "cpplint" },
+      lua = { "luacheck" },
+      html = { "tidy" },
+      css = { "stylelint" },
+      javascript = { "eslint" },
+      py = { "pylint" },
+    }
+    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+      callback = function()
+        require("lint").try_lint()
+      end,
+    })
+  end,
 }
